6f479b37bf3f26ab37a0600bf7dba50c18182011
François Sarradin <fsarradin@gmail.com>
2013-02-26 01:10:22 +0100

    simplified base repo

--- src/main/scala/bearded/bank/base/package.scala
+++ src/main/scala/bearded/bank/base/package.scala
@@ -9,30 +9,22 @@ package object base {
 
   class AccountRepository(private val bankProxies: Map[BankName, BankProxy]) {
 
-    def getAccount(bankName: BankName, accountNumber: AccountNumber): Account = {
-      if (!bankProxies.contains(bankName)) {
-        null
-      }
+    def getAccount(bankName: BankName, accountNumber: AccountNumber): Account =
+      if (!bankProxies.contains(bankName)) null
       else {
         val bankProxy: BankProxy = bankProxies(bankName)
         val account: Account = bankProxy.getAccountByNumber(accountNumber)
         account
       }
-    }
 
   }
 
 
   class BankProxy(private val bankAccounts: Map[AccountNumber, Account]) {
 
-    def getAccountByNumber(accountNumber: AccountNumber): Account = {
-      if (!bankAccounts.contains(accountNumber)) {
-        null
-      }
-      else {
-        bankAccounts(accountNumber)
-      }
-    }
+    def getAccountByNumber(accountNumber: AccountNumber): Account =
+      if (!bankAccounts.contains(accountNumber)) null
+      else bankAccounts(accountNumber)
 
   }
 
