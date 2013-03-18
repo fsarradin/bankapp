a6ffb255590fc243052561e55f3280bf8a14ec22
François Sarradin <fsarradin@gmail.com>
2013-03-19 00:06:22 +0100

    manage exception in AccountRepository

--- src/main/scala/eu/alice/bankapp/bank/AccountRepository.scala
+++ src/main/scala/eu/alice/bankapp/bank/AccountRepository.scala
@@ -6,10 +6,30 @@ class AccountRepository(bankAccessors: Map[String, BankAccessor]) {
 
   def getAccount(bankName: String, accountNumber: String): Account =
   {
-    val bankAccessor: BankAccessor = bankAccessors(bankName)
-    val account: Account = bankAccessor.getAccountByNumber(accountNumber)
+    val bankAccessor: BankAccessor = getBankAccessor(bankName)
+    val account: Account = getAccountByNumber(bankAccessor, accountNumber)
     account
   }
+
+
+  def getBankAccessor(bankName: String): BankAccessor =
+  {
+    if (!bankAccessors.contains(bankName))
+      throw new BankException(s"unknown bank name $bankName")
+    else
+      bankAccessors(bankName)
+  }
+
+  def getAccountByNumber(bankAccessor: BankAccessor, accountNumber: String): Account =
+  {
+    val account: Account = bankAccessor.getAccountByNumber(accountNumber)
+
+    if (account == null)
+      throw new BankException(s"unknown account number $accountNumber")
+    else
+      account
+  }
+
 
 }
 
